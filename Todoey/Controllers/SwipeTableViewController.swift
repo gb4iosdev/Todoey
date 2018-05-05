4a97944bbcfbc7e143751f0a2e124f2becb8237d
Gavin Butler <Gavin@gavins-macbook-air.local>
2018-05-04 20:00:53 -0600

    Remove SwipeCellKit cocoaPod

--- Todoey/Controllers/SwipeTableViewController.swift
+++ Todoey/Controllers/SwipeTableViewController.swift
@@ -7,10 +7,9 @@
 //
 
 import UIKit
-import SwipeCellKit
 import ChameleonFramework
 
-class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegate {
+class SwipeTableViewController: UITableViewController {
     
     var italicTextHeight: CGFloat = 20.0
 
@@ -24,40 +23,18 @@ class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegat
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let localCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SwipeTableViewCell      ///Get a reusable cell from the tableView.
+        let localCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) //as! SwipeTableViewCell      ///Get a reusable cell from the tableView.
         
         if localCell.textLabel?.text == nil {   ///cell is new, not dequeued
             //localCell.backgroundColor = UIColor.randomFlat
-            localCell.delegate = self
+            //localCell.delegate = self
         }
         
         
         
         return localCell
     }
-    
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
-        guard orientation == .right else { return nil }
-        
-        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
-            // handle action by updating model with deletion
-            
-            self.updateModel(at: indexPath)
-            
-//            
-        }
-        
-        // customize the action appearance
-        deleteAction.image = UIImage(named: "delete-icon")
-        
-        return [deleteAction]
-    }
-    
-    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
-        var options = SwipeTableOptions()
-        options.expansionStyle = .destructive
-        return options
-    }
+
     
     func updateModel (at indexPath: IndexPath) {
         //Update our data model.
